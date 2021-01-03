b2a1269bb6425a447461e1a580dca78952547483
wowococo <wowococo@github.com>
2021-01-03 16:46:08 +0800

    review 21

--- leetcode/021 - Merge Two Sorted Lists/021_merge-two-sorted-lists.py
+++ leetcode/021 - Merge Two Sorted Lists/021_merge-two-sorted-lists.py
@@ -17,6 +17,18 @@ class Q021(Solution):
         if l2: l.next = l2
         return dummy.next
 
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = tail = ListNode(0)
+        while l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            tail.next = l1
+            l1 = l1.next
+            tail = tail.next
+        if l1: tail.next = l1
+        if l2: tail.next = l2
+        return dummy.next
+
 def main():
     q = Q021()
     q.set_context(LinkedListContext)
