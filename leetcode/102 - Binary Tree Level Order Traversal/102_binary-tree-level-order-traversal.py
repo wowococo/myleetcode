b766481a98d1beb0bac248f5969c61f162fdecd4
wowococo <1695599369@qq.com>
2021-03-04 21:21:18 +0800

    impl 102 using level traversal

--- leetcode/102 - Binary Tree Level Order Traversal/102_binary-tree-level-order-traversal.py
+++ leetcode/102 - Binary Tree Level Order Traversal/102_binary-tree-level-order-traversal.py
@@ -5,26 +5,29 @@ from leezy.assists import TreeContext, TreeNode
 class Q102(Solution):
     @solution
     def levelOrder(self, root):
-        # import pdb; pdb.set_trace()
-        if not root: return
+        if not root: return []
         res = []
         q = queue.Queue()
         q.put(root)
         while q.qsize():
-            node = q.get()
-            res.append(node.val)
-            if node.left:
-                q.put(node.left)
-            if node.right:
-                q.put(node.right)
+            n = q.qsize()
+            level = []
+            for i in range(n):
+                node = q.get()
+                level.append(node.val)
+                if node.left:
+                    q.put(node.left)
+                if node.right:
+                    q.put(node.right)
+            res.append(level)
         return res
 
         
-
 def main():
     q = Q102()
     q.set_context(TreeContext)
     q.add_case(q.case([3, 9, 20, None, None, 15, 7]))
+    q.add_case(q.case([]))
     q.run()
 
 
