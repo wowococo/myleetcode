2c18ccfb2ff6ab6c7f68ca7af23c5d12b1327677
wowococo <1695599369@qq.com>
2021-03-11 14:38:54 +0800

    impl 105 using recur

--- leetcode/105 - Construct Binary Tree from Preorder and Inorder Traversal/105_construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ leetcode/105 - Construct Binary Tree from Preorder and Inorder Traversal/105_construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,25 +1,18 @@
 from leezy import solution, Solution
 from leezy.assists import TreeContext, TreeNode
-from collections import deque
 
 class Q105(Solution):
     @solution
     def buildTree(self, preorder, inorder):
-        
-        def reconstruct(preorder, inorder, stop):
-            if inorder[0] == stop: return None  # 递归终止条件
-            root = TreeNode(preorder.popleft())     # 创建根结点，并消耗掉先序遍历流中的根
-            print(preorder, '                    ', inorder)
-            root.left = reconstruct(preorder, inorder, root.val)
-                                        # 重建左子树，直到在中序遍历流中遇到当前层的根
-            inorder.popleft()            # 消耗掉中序遍历流中的根
-            root.right = reconstruct(preorder, inorder, stop)
-                                        # 重建右子树，直到在中序遍历流中遇到上层的根
-            return root                  # 返回重建结果
-        
-        return reconstruct(deque(preorder + [None]), deque(inorder + [None]), None)
+        if not preorder: return 
+        root = TreeNode(preorder[0])
+        p = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:p+1], inorder[:p])
+        root.right = self.buildTree(preorder[p+1:], inorder[p+1:])
+        return root
 
-    
+    from collections import deque
+
 
 def main():
     q = Q105()
