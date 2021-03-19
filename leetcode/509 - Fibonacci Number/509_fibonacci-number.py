64ba0feeddd485a0a3952cdd80c5b5db09a864d6
wowococo <1695599369@qq.com>
2021-03-19 09:44:02 +0800

    记忆化递归

--- leetcode/509 - Fibonacci Number/509_fibonacci-number.py
+++ leetcode/509 - Fibonacci Number/509_fibonacci-number.py
@@ -4,11 +4,26 @@ from leezy import solution, Solution
 class Q509(Solution):
     @solution
     def fib(self, n):
+        # 重叠子问题
         if n == 1:
             return 1
         if n == 0:
             return 0
         return self.fib(n-1) + self.fib(n-2)
+    
+    @solution
+    def fib_better(self, n):
+        memo = [0] * (n+1)
+        return self.f(memo, n)
+    
+    def f(self, memo, n):
+        if n == 1: return 1
+        if n == 0: return 0
+        if memo[n] != 0:
+            return memo[n]
+        memo[n] = self.f(memo, n-1) + self.f(memo, n-2)
+        return memo[n]
+    
 
 
 def main():
