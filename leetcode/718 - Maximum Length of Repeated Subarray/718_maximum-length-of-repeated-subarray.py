683fa3f11233345796e54551d18daf2603147906
wowococo <1695599369@qq.com>
2021-03-23 10:04:57 +0800

    add comment

--- leetcode/718 - Maximum Length of Repeated Subarray/718_maximum-length-of-repeated-subarray.py
+++ leetcode/718 - Maximum Length of Repeated Subarray/718_maximum-length-of-repeated-subarray.py
@@ -4,10 +4,11 @@ from leezy import solution, Solution
 class Q718(Solution):
     @solution
     def findLength(self, A, B):
-        # dp[i][j] 表示以A[i-1]，B[j-1]为结尾的子数组的最长公共子数组的长度
-        # i, j 表示两个子数组的长度
+        # dp[i][j]  i, j 表示两个子数组的长度
+        # 表示以A[i-1]，B[j-1]为结尾的子数组的最长公共子数组的长度
+        # 3296 ms 77.72%
         m, n = len(A), len(B)
-        dp = [[-1] * (n+1) for _ in range (m+1)]
+        dp = [[-1] * (n+1) for _ in range(m+1)]
         for i in range(m+1):
             dp[i][0] = 0
         for j in range(n+1):
@@ -18,7 +19,7 @@ class Q718(Solution):
                     dp[i][j] = dp[i-1][j-1] + 1
                 else:
                     dp[i][j] = 0
-            
+        
         return max(map(max, dp))
 
 def main():
