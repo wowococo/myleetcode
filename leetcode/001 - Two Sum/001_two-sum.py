4f0255a7232c04a2ff821d34b4fe7c77e8142b74
wowococo <1695599369@qq.com>
2021-03-29 11:10:37 +0800

    update

--- leetcode/001 - Two Sum/001_two-sum.py
+++ leetcode/001 - Two Sum/001_two-sum.py
@@ -7,11 +7,12 @@ class Q001(Solution):
         # 此题要求返回的是下标，因为不能直接对原数组按照数大小进行排序，因为这样会改变数的位置，
         # 因此有个很巧妙的思路是： 对数组的索引按照每个索引在数组中对应的值的大小，对索引进行排序（默认就是升序）
         if nums is None or len(nums) < 2:
-            return []   
+            return []
         n = len(nums)
         indexs = sorted(range(n), key=lambda i: nums[i])
-        # i，j代表 indexs的下标
-        i, j = 0, len(nums)-1
+        # i，j代表 indexs 的索引  
+        i, j = 0, len(indexs) - 1     # 或者 i, j = 0, len(nums) - 1
+        
         while i < j:
             s = nums[indexs[i]] + nums[indexs[j]]
             if s < target:
@@ -20,8 +21,6 @@ class Q001(Solution):
                 j -= 1
             else:
                 return sorted([indexs[i], indexs[j]])
-        return []
-        
 
 
 def main():
