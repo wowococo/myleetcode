cec5749dfd33330287dc662a36b89b9771dd7bbf
wowococo <1695599369@qq.com>
2021-04-03 17:57:53 +0800

    hashtable and sliding window

--- leetcode/003 - Longest Substring Without Repeating Characters/003_longest-substring-without-repeating-characters.py
+++ leetcode/003 - Longest Substring Without Repeating Characters/003_longest-substring-without-repeating-characters.py
@@ -4,12 +4,22 @@ from leezy import solution, Solution
 class Q003(Solution):
     @solution
     def lengthOfLongestSubstring(self, s):
-        pass
-
+        # hashtable and sliding window, recommend huahua
+        # 72ms 70.58%
+        memo = {}
+        i = 0
+        max_len = 0
+        for j in range(len(s)):
+            if s[j] in memo:
+                i = max(i, memo[s[j]]+1)
+            max_len = max(max_len, j-i+1)
+            memo[s[j]] = j
+        return max_len
 
 def main():
     q = Q003()
-    q.add_case(q.case('abcabcbb'))
+    q.add_case(q.case('abcabcbb').assert_equal(3))
+    q.add_case(q.case('abbcabcbb').assert_equal(3))
     q.run()
 
 if __name__ == '__main__':
