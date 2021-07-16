bb56165da5cc2bf0121361708335ebd7423b90a5
wowococo <1695599369@qq.com>
2021-07-16 12:51:56 +0800

    full Permutations without duplicated numbers

---
+++ leetcode/046 - Permutations/046_permutations.py
@@ -0,0 +1,34 @@
+from leezy import solution, Solution
+
+
+class Q046(Solution):
+    @solution
+    def permute(self, nums):
+        # 32ms 95.48%
+        total, cur = [], []
+        N = len(nums)
+        used = [False] * N
+        def dfs():
+            if len(cur) == N:
+                total.append(cur.copy())
+                return
+            for i in range(N):
+                if used[i]:
+                    continue
+                used[i] = True
+                cur.append(nums[i])
+                dfs()
+                cur.pop()
+                used[i] = False
+    
+        dfs()
+        return total
+
+def main():
+    q = Q046()
+    q.add_case(q.case([1, 2, 3]))
+    q.run()
+
+
+if __name__ == '__main__':
+    main()
