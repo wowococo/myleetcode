bca413aad7e58384056d255925c8f04c36e64365
wowococo <1695599369@qq.com>
2021-07-18 16:23:13 +0800

    add comment

--- leetcode/031 - Next Permutation/031_next-permutation.py
+++ leetcode/031 - Next Permutation/031_next-permutation.py
@@ -4,24 +4,26 @@ from leezy import solution, Solution
 class Q031(Solution):
     @solution
     def nextPermutation(self, nums):
-        # 36ms, 80.84%
+        # 32ms, 91.78%
         N = len(nums)
         if N <= 1:
             return
-        i, j, k = N - 2, N - 1, N - 1
+        i, j, k = N-2, N-1, N-1
         while i >= 0 and nums[i] >= nums[j]:
             i -= 1
             j -= 1
-        if i >= 0:
+        if i >= 0:   # 存在能替换 nums[i] 的 nums[k]， 因此 k 至少为 1，下面 while 循环不用判断 k 是否 >=0
             while nums[i] >= nums[k]:
-                k -= 1
+                k -= 1 
             nums[i], nums[k] = nums[k], nums[i]
         
+        # 使nums[j:] 由降序变为升序排列
         i, j = j, N-1
         while i < j:
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
+
         # return nums
 
 def main():
