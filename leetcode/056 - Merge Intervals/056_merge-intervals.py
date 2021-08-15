ff156e15400952d3c2f7f6bebf89bd639e770f41
wowococo <1695599369@qq.com>
2021-08-15 18:45:10 +0800

    合并区间

--- leetcode/056 - Merge Intervals/056_merge-intervals.py
+++ leetcode/056 - Merge Intervals/056_merge-intervals.py
@@ -4,12 +4,24 @@ from leezy import solution, Solution
 class Q056(Solution):
     @solution
     def merge(self, intervals):
-        pass
-
-
+        # 36ms 90.02%
+        intervals = sorted(intervals)
+        N = len(intervals)
+        ans = [intervals[0]]
+        for i in range(1, N):
+            if intervals[i][0] > ans[-1][1]:
+                ans.append(intervals[i])
+            else:
+                ans[-1] = [ans[-1][0], max(intervals[i][1], ans[-1][1])]
+        return ans
+                
+            
 def main():
     q = Q056()
     q.add_case(q.case([[1, 3], [2, 6], [8, 10], [15, 18]]))
+    q.add_case(q.case([[1,4],[4,5]]))
+    q.add_case(q.case([[1,4],[4,5], [5,6]]))
+    q.add_case(q.case([[1,4],[2,3]]).assert_equal([[1,4]]))
     q.run()
 
 
