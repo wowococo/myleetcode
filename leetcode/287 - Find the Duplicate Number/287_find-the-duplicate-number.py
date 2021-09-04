8f2718e279df41e9cb2db336c32d2bbe7744878e
wowococo <1695599369@qq.com>
2021-09-04 19:22:11 +0800

    find the duplicate number ⭐️ linked list cycle

--- leetcode/287 - Find the Duplicate Number/287_find-the-duplicate-number.py
+++ leetcode/287 - Find the Duplicate Number/287_find-the-duplicate-number.py
@@ -4,13 +4,26 @@ from leezy import solution, Solution
 class Q287(Solution):
     @solution
     def findDuplicate(self, nums):
-        pass
-
-
+        # 转化为找环形链表的环入口点 96ms 75.99%
+        fast = slow = 0
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        while fast != slow:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        
+        cursor = 0
+        while cursor != slow:
+            cursor = nums[cursor]
+            slow = nums[slow]
+        
+        return cursor
 
 def main():
     q = Q287()
-    q.add_case(q.case([1, 3, 4, 2, 2]))
+    q.add_case(q.case([1, 3, 4, 2, 2]).assert_equal(2))
+    q.add_case(q.case([1,1]).assert_equal(1))
+    q.add_case(q.case([3,1,2,3,4]).assert_equal(3))
     q.run()
 
 
