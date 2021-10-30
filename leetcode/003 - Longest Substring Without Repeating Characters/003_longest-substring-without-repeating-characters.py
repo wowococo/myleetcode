05ee1c48bd19bffe438e25d159e2e3b897e93ac9
wowococo <1695599369@qq.com>
2021-10-30 16:25:14 +0800

    review 003 子串->滑动窗口

--- leetcode/003 - Longest Substring Without Repeating Characters/003_longest-substring-without-repeating-characters.py
+++ leetcode/003 - Longest Substring Without Repeating Characters/003_longest-substring-without-repeating-characters.py
@@ -1,7 +1,26 @@
 from leezy import solution, Solution
-
+from collections import defaultdict
 
 class Q003(Solution):
+    @solution
+    def length_of_longest_substring(self, s):
+        # 遇见子串首先要想到滑动窗口,这道题valid是含有重复字符的时候
+        i, j = 0, 0
+        # memo 存储窗口里遇见字符的数量
+        memo = defaultdict(int)
+        res = 0
+        for j in range(len(s)):
+            right = s[j]
+            memo[right] += 1
+            while memo[right] > 1:
+                left = s[i]
+                memo[left] -= 1
+                i += 1
+            
+            # 因为求的是最长子串，所以在每次j 向右扩展窗口的时候判断一下 res
+            res = max(res, j-i+1)
+        return res
+
     @solution
     def lengthOfLongestSubstring(self, s):
         # hashtable and sliding window, recommend huahua
