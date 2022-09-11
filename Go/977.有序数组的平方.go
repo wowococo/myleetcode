4cd5501ee73668bb4711dc3891ba2bcd130df67b
wowococo <1695599369@qq.com>
2022-09-11 11:49:21 +0800

    双指针 O(n)

--- Go/977.有序数组的平方.go
+++ Go/977.有序数组的平方.go
@@ -6,8 +6,27 @@
 package main
 
 // @lc code=start
-// func sortedSquares(nums []int) []int {
+func sortedSquares(nums []int) []int {
+	index := len(nums) - 1
+	i, j := 0, index
+	result := make([]int, len(nums))
+	for i <= j {
+		left, right := square(nums[i]), square(nums[j])
+		if left < right {
+			result[index] = right
+			j--
+		} else {
+			result[index] = left
+			i++
+		}
+		index--
+	}
 
-// }
+	return result
+}
+
+func square(num int) int {
+	return num * num
+}
 
 // @lc code=end
