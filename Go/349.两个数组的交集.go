cae307d80fd013f2309b42fd7379a6fcfdac8c41
wowococo <1695599369@qq.com>
2022-10-07 17:12:34 +0800

    新增哈希表用数组的方式

--- Go/349.两个数组的交集.go
+++ Go/349.两个数组的交集.go
@@ -12,7 +12,7 @@ package main
 // 思路：将 nums1 的数存到一个哈希表里，遍历nums2，判断
 // nums2 的每个元素是否在哈希表里，如果在的话就加入到
 // 结果哈希表里(因为要去重)。
-func intersection(nums1 []int, nums2 []int) []int {
+func intersectionSet(nums1 []int, nums2 []int) []int {
 	var result []int
 	numsMap := make(map[int]int, len(nums1))
 	resultMap := make(map[int]int)
@@ -34,4 +34,32 @@ func intersection(nums1 []int, nums2 []int) []int {
 	return result
 }
 
+// 数组形式, 存储 nums1 的用数组。
+// 也可以存储 nums1 的哈希表用 set，存储 result 的用数组
+func intersection(nums1 []int, nums2 []int) []int {
+	numsSet := make([]int, 1001)
+	resultSet := make(map[int]int, 0)
+	var result []int
+	for _, num := range nums1 {
+		numsSet[num] = 1
+	}
+
+	for _, num := range nums2 {
+		if numsSet[num] == 1 {
+			resultSet[num] = 1
+		}
+	}
+
+	for num := range resultSet {
+		result = append(result, num)
+	}
+
+	return result
+}
+
 // @lc code=end
+// func main() {
+// 	nums1 := []int{1, 2, 2, 1}
+// 	nums2 := []int{2, 2}
+// 	intersection(nums1, nums2)
+// }
