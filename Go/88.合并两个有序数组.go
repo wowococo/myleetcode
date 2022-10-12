46d3d37e408be2f8390809aad55e8b8bfde79622
wowococo <1695599369@qq.com>
2022-10-12 19:59:32 +0800

    使用一个额外的数组存储nums1的实际元素，遍历覆盖nums1

---
+++ Go/88.合并两个有序数组.go
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=golang
+ *
+ * [88] 合并两个有序数组
+ */
+package main
+
+// @lc code=start
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	// 使用一个额外的数组
+	nums1_copy := make([]int, m)
+	copy(nums1_copy, nums1[:m])
+
+	var i, j, k int
+	for i < m && j < n {
+		if nums1_copy[i] < nums2[j] {
+			nums1[k] = nums1_copy[i]
+			i++
+		} else {
+			nums1[k] = nums2[j]
+			j++
+		}
+
+		k++
+	}
+
+	for i < m && k < m+n {
+		nums1[k] = nums1_copy[i]
+		i++
+		k++
+	}
+
+	for j < n && k < m+n {
+		nums1[k] = nums2[j]
+		j++
+		k++
+	}
+}
+
+// @lc code=end
+
+func main() {
+	merge([]int{1,2,3,0,0,0}, 3, []int{2,5,6}, 3)
+}
