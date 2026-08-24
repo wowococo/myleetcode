2f2bed0982c9adccacb38620d081d39111473755
wowococo <1695599369@qq.com>
2026-08-24 18:23:42 +0800

    update 142.环形链表-ii---快慢指针都从 head 开始，相遇后 fast 重置到 head

--- Go/142.环形链表-ii.go
+++ Go/142.环形链表-ii.go
@@ -16,26 +16,28 @@
 package main
 
 func detectCycle(head *ListNode) *ListNode {
-    // 快慢指针，快指针的速度是慢指针的2倍
-	fast, slow := head, head
-	for fast != nil && fast.Next != nil{
-		fast = fast.Next.Next
+	// 快慢指针，快指针的速度是慢指针的2倍
+	// 这是一道数学题，结论是：
+	// 当相遇后，快指针重新到 head 位置，走同样的距离就到入环点了
+	// 要考虑到假设只有一个节点的边界情况
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
 		slow = slow.Next
+		fast = fast.Next.Next
 
-		// 表示存在环
+		// 相遇之后 fast 到 head 位置，fast 和 slow 再重新走相同的距离
 		if fast == slow {
 			fast = head
-			for slow != fast {
+			// 进入循环前有可能已经到达入口了，所以不能用 fast != nil 判断
+			for fast != slow {
 				fast = fast.Next
 				slow = slow.Next
 			}
-
 			return fast
 		}
 	}
 
 	return nil
-
 }
-// @lc code=end
 
+// @lc code=end
