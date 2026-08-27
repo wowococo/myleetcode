fa5861d3ad51b62b8961ca48539a8ad5e7589caf
wowococo <1695599369@qq.com>
2026-08-27 17:39:08 +0800

    update 23.合并 k 个升序链表--增加迭代做法

--- Go/23.合并k个升序链表.go
+++ Go/23.合并k个升序链表.go
@@ -15,7 +15,71 @@ package main
  * }
  */
 
+//  迭代做法
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// 迭代做法，将合并 k 个链表转化成多组 “合并 2 个升序链表”
+// 时间复杂度：O（n * logk） n是每个链表的平均节点个数，k 是 k 个链表
 func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+
+	// 如果长度大于 1，也就是至少 2 个，将多个拆成 2 个，2 个，2个
+	for len(lists) > 1 {
+		// 临时存这次拆分后合并后的数组
+		tmp := make([]*ListNode, 0)
+		for i := 0; i < len(lists); i += 2 {
+			list1 := lists[i]
+			var list2 *ListNode
+			if i+1 < len(lists) {
+				list2 = lists[i+1]
+			}
+
+			tmp = append(tmp, merge2Lists(list1, list2))
+		}
+
+		// 将 tmp 新合并后的数组赋值给 lists，进行下一次两两合并
+		lists = tmp
+	}
+
+	// 直至跳出循环时，更新后的lists长度等于 1，说明是合并好的结果
+	return lists[0]
+}
+
+// 迭代写法实现合并 2 个升序链表，虚拟节点 dummy 加尾节点 tail
+func merge2Lists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	tail := dummy
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			tail.Next = list1
+			list1 = list1.Next
+		} else {
+			tail.Next = list2
+			list2 = list2.Next
+		}
+
+		// tail 始终指向链表的最后一个节点
+		tail = tail.Next
+	}
+
+	if list1 != nil {
+		tail.Next = list1
+	} else if list2 != nil {
+		tail.Next = list2
+	}
+
+	return dummy.Next
+}
+
+// 递归做法
+func mergeKListsRecur(lists []*ListNode) *ListNode {
 	n := len(lists)
 	if n < 1 {
 		return nil
@@ -25,12 +89,12 @@ func mergeKLists(lists []*ListNode) *ListNode {
 	}
 
 	num := n / 2
-	left := mergeKLists(lists[:num])
-	right := mergeKLists(lists[num:])
-	return mergeTwoList(left, right)
+	left := mergeKListsRecur(lists[:num])
+	right := mergeKListsRecur(lists[num:])
+	return mergeTwoListRecur(left, right)
 }
 
-func mergeTwoList(list1 *ListNode, list2 *ListNode) *ListNode {
+func mergeTwoListRecur(list1 *ListNode, list2 *ListNode) *ListNode {
 	if list1 == nil {
 		return list2
 	}
@@ -38,11 +102,11 @@ func mergeTwoList(list1 *ListNode, list2 *ListNode) *ListNode {
 		return list1
 	}
 	if list1.Val < list2.Val {
-		list1.Next = mergeTwoList(list1.Next, list2)
+		list1.Next = mergeTwoListRecur(list1.Next, list2)
 		return list1
 	}
 
-	list2.Next = mergeTwoList(list1, list2.Next)
+	list2.Next = mergeTwoListRecur(list1, list2.Next)
 	return list2
 }
 
